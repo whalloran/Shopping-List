da42c94d18c1dc0a034db1f45e9a87d9d75d5365
whalloran <66441541+whalloran@users.noreply.github.com>
2020-06-29 21:00:14 -0400

    Added Features
    Added print list feature
    Added Remove Last Feature
    Fixed bug in Bottom / Top setting

--- list.js
+++ list.js
@@ -1,7 +1,14 @@
 
 var myList = [];
 
-//  Pressing the Enter key clicks Add to List button
+//Clears array and returns settings to default on page load
+function pageLoad() {
+	document.getElementById('bottom').checked = true;
+	document.getElementById('listItem').focus();
+	myList = [];
+}
+
+//  Clicks Add to List button when Enter key is pressed while input is in focus
 var input = document.getElementById('listItem');
 input.addEventListener("keydown", function(event) {
   if (event.keyCode === 13) {
@@ -17,31 +24,50 @@ function addToList() {
 	//  Prevents blank inputs
 	if (newItem != "<li></li>") {
 
-	//  Adds new item to top of list 
-	if (document.getElementById('top').checked) {
-	myList.unshift(newItem);
+		//  Adds new item to bottom of list (default)
+		if (document.getElementById('bottom').checked) {
+			myList.push(newItem); 	
+		} 
+
+		//  Adds new item to top of list if selected in settings
+		else if (document.getElementById('top').checked) {
+			myList.unshift(newItem);
+		}
 	}
 
-	//  Adds new item to bottom of list
-	else if (document.getElementById('bottom').checked) {
-		myList.push(newItem); 	
-	}  
-
-}
 	document.getElementById('list-content').innerHTML =  myList.join("");  // Output updated list
-    	document.getElementById('form').reset();	// Reset Form - clear listItem input
-    	document.getElementById('listItem').focus();  // Move cursor to listItem input
-};
-
-function removeFromList() {
-	document.getElementById('removeItem');
+    document.getElementById('form').reset();	// Reset Form - clear listItem input
+    document.getElementById('listItem').focus();  // Move cursor to listItem input
 }
 
-document.getElementById('bottom').checked = true;
-
-/*  Print button only prints the shopping list  */
-
-function printList() {
-        $("#shoppingList").addClass("printable");
-        window.print();
+// Removes last item from list
+function removeLast() {
+	myList.pop();
+	document.getElementById('list-content').innerHTML = myList.join("");
 }
+
+// Toggle settings menu
+function settings() {
+	var set = document.getElementById('settings');
+	if (set.style.display === "block") {
+		set.style.display = "none";
+		document.getElementById('settings-div').innerHTML = "<a href='#' id='settings-toggle' onclick='settings()'>+ Settings</a>";
+	}
+
+	else {
+		set.style.display = "block";
+		document.getElementById('settings-div').innerHTML = "<a href='#' id='settings-toggle' onclick='settings()'>Hide Settings</a>";
+	}
+}
+
+// Print Shopping List
+function printList() { 
+			var divContents = document.getElementById('shoppingList').innerHTML; 
+			var a = window.open('', '', 'height=500, width=500'); 
+			a.document.write('<html><head><title>Print Shopping List</title><link rel="stylesheet" type="text/css" href="style.css"></head>');
+			a.document.write('<body style="background-color: #fff;"><div id="printView">');  
+			a.document.write(divContents); 
+			a.document.write('</div></body></html>'); 
+			a.document.close(); 
+			a.print(); 
+		} 
