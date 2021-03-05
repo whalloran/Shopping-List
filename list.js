3aa1c0e42439c1caceb83ebaa9ab68e442e44400
whalloran <66441541+whalloran@users.noreply.github.com>
2021-03-04 20:08:56 -0500

    JS update

--- list.js
+++ list.js
@@ -1,73 +1,103 @@
 
 var myList = [];
 
-//  
+// Clears list and returns settings to default on page load
+let pageLoad = () => {
+	document.getElementById('bottom').checked = true;
+	document.getElementById('capitalize').checked = true;
+	document.getElementById('listItem').focus();
+	document.getElementById('listItem').value = "";
+	myList = [];
+}
+
+//  Clicks Add to List button when ENTER key is pressed while input is in focus
 var input = document.getElementById('listItem');
-input.addEventListener("keydown", function(event) {
-  if (event.keyCode === 13) {
-   event.preventDefault();
-   document.getElementById('addBtn').click();
+input.addEventListener("keydown", (event) => {
+	if (event.keyCode === 13) {
+	event.preventDefault();
+	document.getElementById('addBtn').click();
   }
 });
 
-function addToList() {
+//  New item is formatted to add to ordered list
+let addToList = () => {
 	var newItem = "<li>" + document.getElementById('listItem').value + "</li>";
-   
-   if (newItem != "<li></li>") {
 
-   //  Adds new item to top of list 
-   if (document.getElementById('top').checked) {
-	myList.unshift(newItem);
+	//  Prevents blank inputs
+	if (newItem != "<li></li>") {
+
+		//  Adds new item to bottom of list (default)
+		if (document.getElementById('bottom').checked) {
+			myList.push(newItem); 	
+		} 
+
+		//  Adds new item to top of list if selected in settings
+		else if (document.getElementById('top').checked) {
+			myList.unshift(newItem);
+		}
 	}
-
-	//  Adds new item to bottom of list
-	else if (document.getElementById('bottom').checked) {
-	myList.push(newItem); 	
-	}  
-
-	}
-
-    document.getElementById('list-content').innerHTML =  myList.join("");  // Output updated list
+	document.getElementById('list-content').innerHTML =  myList.join("");  // Output updated list
     document.getElementById('form').reset();	// Reset Form - clear listItem input
     document.getElementById('listItem').focus();  // Move cursor to listItem input
-};
-
-function removeFromList() {
-	document.getElementById('removeItem');
 }
 
-document.getElementById('bottom').checked = true;
-
-/*
-		Input to add item to list
-		Assigns number to each item based on index (x = [index] + 1)
-		Cross item off the list... strikethrough or check
-		Remove item from list
-		Print List 
-
-
-		Create "X Remove" link next to each item to remove from list
-
-
-
-*/
-
-
-
-/*  Print button only prints the shopping list  */
-
-function printList() {
-        $("#shoppingList").addClass("printable");
-        window.print();
-      }
-
-
-
-/*
-if(document.getElementById('gender_Male').checked) {
-  //Male radio button is checked
-}else if(document.getElementById('gender_Female').checked) {
-  //Female radio button is checked
+// Removes last item from list
+let removeLast = () => {
+	myList.pop();
+	document.getElementById('list-content').innerHTML = myList.join("");
 }
 
-*/
+// Toggle settings menu
+$("#settings-toggle").click( () => {
+	$("#settings").toggle();
+	$("#settings-toggle").text($("#settings-toggle").text() == '+ Settings' ? 'Hide Settings' : '+ Settings');
+});
+
+// Print Shopping List
+let printList = () => { 
+	var divContents = document.getElementById('shoppingList').innerHTML; 
+	var printPage = window.open('', '', 'height=500, width=500'); 
+	printPage.document.write('<html><head><title>Print Shopping List</title><link rel="stylesheet" type="text/css" href="style.css"></head>');
+	printPage.document.write('<body style="background-color: #fff;"><div id="printView">');  
+	printPage.document.write(divContents); 
+	printPage.document.write('</div></body></html>'); 
+	printPage.document.close(); 
+	printPage.print(); 
+}
+
+let updateSettings = () => {
+	
+	// Settings - change case
+	var caps = document.getElementById('capitalize').checked;
+	var upper = document.getElementById('uppercase').checked;
+	var lower = document.getElementById('lowercase').checked;
+
+	if (upper == true) {
+		document.getElementById('list-content').style.textTransform = "uppercase";
+	}
+
+	else if (lower == true) {
+		document.getElementById('list-content').style.textTransform = "lowercase";
+	}
+
+	else {
+		document.getElementById('list-content').style.textTransform = "capitalize";
+	}
+
+	// Settings - change font
+	var fontIndieFlower = document.getElementById('indieFlowerFont').checked;
+	var fontArial = document.getElementById('arialFont').checked;
+	var fontVerdana = document.getElementById('verdanaFont').checked;
+	
+	if (fontArial == true) {
+		document.getElementById('list-content').style.fontFamily = "Arial, sans-serif, serif";
+	}
+
+	else if (fontVerdana == true) {
+		document.getElementById('list-content').style.fontFamily = "Verdana, sans-serif, serif";
+	}
+
+	else {
+		document.getElementById('list-content').style.fontFamily = "'Indie Flower', cursive";
+	}	
+}
